de7a14b7125e058731855dc14c947fabb75e3e48
akanscode <akanscoded@gmail.com>
2022-11-11 16:42:08 +0000

    first commit

---
+++ src/Components/Common/Data/data.jsx
@@ -0,0 +1,97 @@
+export const data = [
+  {
+    title: "Gas & Energy",
+    text: " Run your trading operations with ease. Either as a Trader re-stocking or as a company procuring, the Product Leasing programme (PLP) is a uniquely structured Buy-Now,Pay-Later offering enabling efficient bulk and retail trading. TradeGrid supports a wide network of independent dealers across a fast-growing number of African countries, providing B2B e-commerce, Logistics and Financing solutions. (Download Brochure)",
+    list1: "Industrial supplies & Chemicals",
+    list2: "Construction & Building Materials",
+    cover: "https://www.thetradegrid.com/assets/images/gas3.png",
+  },
+  {
+    title: "Industrial supplies & Chemicals",
+    text: "Reshape buying for your organization. Our B2B procurement platform allows companies access more supplier options, as more vendors are supported to meet traditional terms through TradeGrid’s invoice financing and flexible payment solutions",
+    list1: "Construction & Building Materials",
+    list2: "Gas & Energy",
+    cover: "https://www.thetradegrid.com/assets/images/gas2.png",
+  },
+  {
+    title: "Construction & Building Materials",
+    text: "Streamline procurement for your construction projects or material outlets. Through our partnership with CinderBuild® – Africa’s largest B2B platform for Construction materials, we offer Trade solutions and BNPL financing to thousands of retailers and bulk consumers, while collaborating with trusted brands such like Dangote Cement, Schneider Electric, Lafarge, Cutix Cables and ABB to ensure authentic supplies at great prices.",
+    list1: " Gas & Energy",
+    list2: " Industrial supplies & Chemicals",
+    cover: "https://www.thetradegrid.com/assets/images/gas.png",
+  },
+];
+export const testimonial = [
+  {
+    text: "My station has received 5 trucks of PMS within 2months of being a part of TradeGrid’s “Buy Now, Pay Later” Plan!. I am currently on the 5th Cycle of the PLP. The PMS was totally FREE and I paid back as I sold the product.All I did, was to apply on the App and submitted my documents to qualify and my station has been running efficiently since then. TradeGrid is trustworthy and reliable.”",
+    cover: "https://www.thetradegrid.com/assets/images/profile.svg",
+    customerName: "Fadeyi Akinjide",
+    companyName: "SIFEM Global Resources.",
+  },
+  {
+    text: " I started with 11,000L and today I am currently on 33,000L!. TradeGrid delivered PMS to me after inspecting traffic at my station. I sold the product quickly and paid back on time!. They believed in me and I am now on Cycle 3 with petroleum funding of over 10million naira from TradeGrid!. Thanks to the TradeGrid Marketing team that came to Ogun State!.",
+    cover: "https://www.thetradegrid.com/assets/images/profile2.svg",
+    customerName: "Tunde Lawal",
+    companyName: "Tunlad Oil and Gas, Ogun State.",
+  },
+  {
+    text: " I started with 11,000L and today I am currently on 33,000L!. TradeGrid delivered PMS to me after inspecting traffic at my station. I sold the product quickly and paid back on time!. They believed in me and I am now on Cycle 3 with petroleum funding of over 10million naira from TradeGrid!. Thanks to the TradeGrid Marketing team that came to Ogun State!.",
+    cover:
+      "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    customerName: "Adebisi ola",
+    companyName: "Olamighy Oil and Gas, Ogun State.",
+  },
+];
+export const logo = [
+  {
+    cover: "",
+  },
+  {
+    cover: "../../../assests/logo1.png",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/oando.svg",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/mrs.svg",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/oryx.svg",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/eterna.svg",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/rubis.svg",
+  },
+  {
+    cover: "https://www.thetradegrid.com/assets/images/pinnacle.svg",
+  },
+];
+export const faq = [
+  {
+    title: "How long is your delivery time?",
+    desc: "Your product, your terms. You get to agree on the delivery time/date with the seller.",
+  },
+  {
+    title: "hat guarantee can I expect from TradeGrid?",
+    desc: "We partner with verified suppliers in your region to provide you quality products at the very best price. We verify that products are delivered in satisfactory condition before buyers' funds are released",
+  },
+  {
+    title:
+      "Are there any other hidden cost or charges for accessing the marketplace?",
+    desc: " Your product, your terms. You get to agree on the delivery time/date with the seller.",
+  },
+  {
+    title: "Do I need to get verified before I begin to sell?",
+    desc: "Your product, your terms. You get to agree on the delivery time/date with the seller.",
+  },
+  {
+    title: "What payment methods do you accept?",
+    desc: "Your product, your terms. You get to agree on the delivery time/date with the seller.",
+  },
+  {
+    title: "How It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+];
